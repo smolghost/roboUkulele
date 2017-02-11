225520fcf49a4548a69589ebd80552159a944aaa
smolghost <eqgrover@gmail.com>
2017-02-11 11:35:07 -0500

    code controlling servo for strumming

---
+++ servo.ino
@@ -0,0 +1,26 @@
+#include <Servo.h> // Include library
+
+Servo myservo; // Make new servo object
+
+int pos = 0; // Store the position
+
+void setup() {
+  
+myservo.attach(9); // attaches the servo on pin 9 to the servo object
+  }
+
+void loop() {
+
+for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees 
+  // in steps of 1 degree
+  myservo.write(pos); // tell servo to go to position in variable 'pos'
+  delay(5); // waits 15ms for the servo to reach the position
+
+    }
+
+for (pos = 180; pos >= 0; pos -= 5) { // goes from 180 degrees to 0 degrees
+  myservo.write(pos); // tell servo to go to position in variable 'pos'
+  delay(3); // waits 15ms for the servo to reach the position
+
+  }
+}
